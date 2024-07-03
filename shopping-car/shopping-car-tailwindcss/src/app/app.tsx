47381809c649979d64b8d62090b675f62b0eb8e5
aiyoudiao <newdiao@163.com>
2024-07-03 16:33:07 +0800

    feat(nx): Added Nx Cloud token to your nx.json
              To connect your workspace to Nx Cloud, push your repository
              to your git hosting provider and go to the following URL:
    
              https://cloud.nx.app/connect/FtvcHnT1T4

---
+++ shopping-car/shopping-car-tailwindcss/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="shopping-car-tailwindcss" />
+    </div>
+  );
+}
+
+export default App;
